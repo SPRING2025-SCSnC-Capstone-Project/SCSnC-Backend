54d2461d4e1acfd70cf5d7486d65942488548d2e
StarryFolf <dangbao2003lqd@gmail.com>
2025-01-21 10:15:19 +0700

    Init Repo

---
+++ src/Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using Application.Common.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext, IApplicationDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
