31029918779b7e72191496dabf4a973a9fdaabde
tri <trihuynh1811@gmail.com>
2025-05-25 15:38:52 +0700

    minor changes

---
+++ src/Infrastructure/Persistence/Migrations/20250525022658_Add_IsAvailable_To_WorkspaceTypeBranch.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_IsAvailable_To_WorkspaceTypeBranch : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAvailable",
+                table: "WorkspaceTypeAtBranches",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAvailable",
+                table: "WorkspaceTypeAtBranches");
+        }
+    }
+}
